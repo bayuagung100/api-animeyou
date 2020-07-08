038bfac4956c46729363776259b658a552dc4151
bayuagung100 <bayuagung100@gmail.com>
2020-07-08 23:25:55 +0700

    config conn mysql

--- routes/config.js
+++ routes/config.js
@@ -1,10 +1,19 @@
 var mysqli = require('mysql');
 
+//local
+// var conn = mysqli.createConnection({
+//     host: 'localhost',
+//     user: 'root',
+//     password: '',
+//     database: 'api-animeyou'
+// });
+
+//rumahweb
 var conn = mysqli.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'api-animeyou'
+    host: '103.247.9.85',
+    user: 'quif4942_bayu',
+    password: 'bayuagung123',
+    database: 'quif4942_api-animeyou'
 });
 
 conn.connect(function (err) {
