4b73d28f9c89d68e7ed34f36cedef763cac067da
bayuagung100 <bayuagung100@gmail.com>
2020-07-09 00:15:28 +0700

    tambah koneksi hostinger

--- routes/config.js
+++ routes/config.js
@@ -9,11 +9,19 @@ var mysqli = require('mysql');
 // });
 
 //rumahweb
+// var conn = mysqli.createConnection({
+//     host: '103.247.9.85',
+//     user: 'quif4942_bayu',
+//     password: 'bayuagung123',
+//     database: 'quif4942_api-animeyou'
+// });
+
+//hostinger
 var conn = mysqli.createConnection({
-    host: '103.247.9.85',
-    user: 'quif4942_bayu',
-    password: 'bayuagung123',
-    database: 'quif4942_api-animeyou'
+    host: 'sql261.main-hosting.eu',
+    user: 'u328098603_bayu',
+    password: 'Bayuagung@123',
+    database: 'u328098603_api_animeyou'
 });
 
 conn.connect(function (err) {
